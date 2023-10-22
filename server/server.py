411f4734d77b7959ffe060288cdef25ec36df437
sarahfrk <sarahfrk28@gmail.com>
2023-10-22 13:14:06 +0100

    last mod

---
+++ server/server.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import util
+app = Flask(__name__)
+
+
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    return response
+
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form('total_sqft'))
+    location = float(request.form('location'))
+    bhk = float(request.form('bhk'))
+    bath = float(request.form('bath'))
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+
+    return response
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("Starting Flask")
+    util.load_saved_artifacts()
+    app.run()
